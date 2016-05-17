782692bc97a66e13beb2cb4c6d21f0c1574389d5
EvgenyZubatov <evgeny.zubatov@gmail.com>
2016-05-17 00:23:07 -0700

    First checkin

---
+++ src/main/java/BaggedClassifier.java
@@ -0,0 +1,93 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import weka.classifiers.Classifier;
+import weka.core.Attribute;
+import weka.core.Capabilities;
+import weka.core.Capabilities.Capability;
+import weka.core.Instance;
+import weka.core.Instances;
+
+public class BaggedClassifier<T extends Classifier> extends Classifier {
+
+	/** for serialization */
+	static final long serialVersionUID = -2693678647096322562L;
+
+	private List<T> m_forrest;
+	private Class<T> m_classFactory;
+	private int m_samplingCount;
+
+	public BaggedClassifier(int samplingCount, Class<T> cls) {
+		m_classFactory = cls;
+		m_samplingCount = samplingCount;
+		
+		m_forrest = new ArrayList();
+	}
+
+	public Capabilities getCapabilities() {
+		Capabilities result = super.getCapabilities();
+		result.disableAll();
+
+		// attributes
+		result.enable(Capability.NOMINAL_ATTRIBUTES);
+
+		result.enable(Capability.MISSING_VALUES);
+
+		// class
+		result.enable(Capability.NOMINAL_CLASS);
+		
+		// instances
+		result.setMinimumNumberInstances(0);
+
+		return result;
+	}
+
+	public void buildClassifier(Instances data) throws Exception {
+		// can classifier handle the data?
+		getCapabilities().testWithFail(data);
+
+		// remove instances with missing class
+		data = new Instances(data);
+		data.deleteWithMissingClass();
+
+		Random rnd = new Random(System.currentTimeMillis());
+		for (int i = 0; i < m_samplingCount; i++) {
+			Instances newDataSet = data;
+			
+			if (i != 0) {
+				newDataSet = new Instances(data, data.numInstances());
+				ShuffleData(data, newDataSet, rnd.nextLong());
+			}
+			
+			T c = m_classFactory.newInstance();
+			c.buildClassifier(newDataSet);
+			
+			m_forrest.add(c);
+		}
+	}
+
+	public double classifyInstance(Instance instance) throws Exception {
+		double[] classes = new double[m_samplingCount];
+		double sum = 0.0;
+		for (int i = 0; i < m_samplingCount; i++) {
+			classes[i] = m_forrest.get(i).classifyInstance(instance);
+			sum += classes[i];
+		}
+
+		return (double)Math.round(sum/(double)m_samplingCount);
+	}
+
+	public String convertClassValueToString(Attribute classAttribute, double classValue) {
+		return classAttribute.value((int) classValue);
+	}
+	
+	private void ShuffleData(Instances data, Instances newData, long seed) {
+		Random rnd = new Random(seed);
+		int size = data.numInstances();
+		for (int i = 0; i < size; i++) {
+			int idx = rnd.nextInt(size);
+			newData.add(data.instance(idx));
+		}
+	}
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,123 @@
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Scanner;
+
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.Discretize;
+//import weka.filters.unsupervised.attribute.Discretize;
+//import weka.filters.unsupervised.attribute.NumericToNominal;
+
+/*
+ * Please replace ARFF_TEST_FILE_PATH and ARFF_TRAINING_FILE_PATH with path to training and test data
+ * Please replace CONFIDENCE with required confidence level (ex 0.99 for 99%)
+ * */
+public class Main {
+
+	private static final String ROOT = "C:\\Users\\Eugene\\Documents\\PMP\\Machine Learning\\Lab3\\Task2\\"; 
+	
+    private static final String TEST_FILE_PATH = ROOT + "pima-indians-diabetes.test";
+    private static final String TRAINING_FILE_PATH = ROOT  + "pima-indians-diabetes.train";
+	
+    private static final String ARFF_TEST_FILE_PATH = TEST_FILE_PATH + ".arff";
+    private static final String ARFF_TRAINING_FILE_PATH = TRAINING_FILE_PATH + ".arff";
+    
+    private static int SamplingCount = 1; 
+    private static Discretize filter = null;
+	
+    public static void main(String[] args) throws Exception {
+        Instances trainingInstances = LoadFile(TRAINING_FILE_PATH, ARFF_TRAINING_FILE_PATH);
+        
+        System.out.println("Building Bagged ID3 from training data...");
+        
+        BaggedClassifier<ID3Chi> id3 = new BaggedClassifier(SamplingCount, ID3Chi.class);
+        id3.buildClassifier(trainingInstances);
+        
+        System.out.println("Completed ID3 tree training");
+        System.out.println(id3.toString());
+
+        Instances testInstances = LoadFile(TEST_FILE_PATH, ARFF_TEST_FILE_PATH);
+        
+        System.out.println("Running ID3 on test data");
+
+        int hit = 0;
+        for(Enumeration<Instance> e = testInstances.enumerateInstances(); e.hasMoreElements();){
+        	Instance i = e.nextElement();
+        	double d = id3.classifyInstance(i);
+        	String c = id3.convertClassValueToString(testInstances.classAttribute(), d);
+        	if (d == i.classValue()) {
+        		hit++;
+        	}
+        }
+        System.out.println("Accuracy on test data is: " + (double)hit*100/testInstances.numInstances() + "%");
+
+        System.out.println("Running ID3 on training data");
+
+        int hit2 = 0;
+        for(Enumeration<Instance> e = trainingInstances.enumerateInstances(); e.hasMoreElements();){
+        	Instance i = e.nextElement();
+        	double d = id3.classifyInstance(i);
+        	String c = id3.convertClassValueToString(testInstances.classAttribute(), d);
+        	if (d == i.classValue()) {
+        		hit2++;
+        	}
+        }
+        System.out.println("Accuracy on training data is: " + (double)hit2*100/trainingInstances.numInstances() + "%");
+    }
+    
+    private static Instances LoadFile(String path, String arffPath) throws Exception {
+    	
+    	java.nio.file.Path fPath = java.nio.file.Paths.get(path);
+    	java.nio.file.Path fArffPath = java.nio.file.Paths.get(arffPath);
+    	
+    	List<String> header = new ArrayList<String>();
+    	String headerText = new Scanner(Main.class.getResourceAsStream("header.txt"), "UTF-8").useDelimiter("\\A").next();
+    	header.add(headerText);
+    	Files.write(fArffPath, header, Charset.defaultCharset(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+    	
+    	List<String> lines = Files.readAllLines(fPath);
+    	Files.write(fArffPath, lines, Charset.defaultCharset(), StandardOpenOption.APPEND, StandardOpenOption.WRITE);    	
+
+        ArffLoader arffTestLoader = new ArffLoader();
+
+        File datasetFile = new File(arffPath);
+        arffTestLoader.setFile(datasetFile);
+
+        Instances attributes = arffTestLoader.getStructure();
+        Instances dataInstances = arffTestLoader.getDataSet();
+        
+        for (Enumeration<Attribute> a = attributes.enumerateAttributes(); a.hasMoreElements();){
+        	Attribute attr = a.nextElement();
+        	System.out.println("Attribute: " + attr.index() + ": type: " + attr.type() + ": " + attr);
+        }
+       
+        dataInstances.setClassIndex(dataInstances.numAttributes()-1);
+        
+        if (filter == null) {
+	        filter = new Discretize();
+	        filter.setInputFormat(dataInstances);
+        }
+        
+        dataInstances = Filter.useFilter(dataInstances, filter);
+        
+        saveCopy(dataInstances, arffPath + "_2");
+        
+        return dataInstances;
+    }
+    
+    protected static void saveCopy(Instances data, String path) throws Exception {
+    	java.nio.file.Path fPath = java.nio.file.Paths.get(path);
+    	List<String> text = new ArrayList<String>();
+    	text.add(data.toString());
+    	Files.write(fPath, text, Charset.defaultCharset(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
+    }    
+}
